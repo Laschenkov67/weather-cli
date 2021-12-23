20a4a1dee86384258b993d0282a92af3c6ae3412
Artem Laschenkov <laschenkov59@yandex.ru>
2021-12-23 18:33:14 +0300

    Взаимодействие с API

--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+		printError('Не передан token');
+		return;
+	}
 	try {
-		await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранён');
 	} catch (e) {
 		printError(e.message);
@@ -26,6 +31,7 @@ const initCLI = () => {
 		// Сохранить токен
         return saveToken(args.t);
 	}
+    getWeather('moscow');
 	// Вывести погоду
 };
 
