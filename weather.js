534ed8c6266995003eeb91c0135da87977ae82cf
Artem Laschenkov <laschenkov59@yandex.ru>
2021-12-23 18:43:51 +0300

    Завершение приложения

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -17,10 +17,24 @@ const saveToken = async (token) => {
 	}
 }
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
 const getForcast = async () => {
 	try {
-		const weather = await getWeather(process.env.CITY);
-		console.log(weather); // Красивый вывод погоды
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
 	} catch (e) {
 		if (e?.response?.status == 404) {
 			printError('Неверно указан город');
@@ -37,17 +51,20 @@ const initCLI = () => {
 	console.log(args);
 	if (args.h) {
 		// Вывод help
-        printHelp();
+        return printHelp();
 	}
+
 	if (args.s) {
 		// Сохранить город
+        return saveCity(args.s);
 	}
+
 	if (args.t) {
 		// Сохранить токен
         return saveToken(args.t);
 	}
-    getWeather('moscow');
-    getForcast();
+
+    return getForcast();
 	// Вывести погоду
 };
 
